59d0a35542c43e2030b8f65af904d6c55b81204f
Mark Sirany <marksirany@me.com>
2020-01-08 12:40:10 -0600

    created burgers database and table for burgers

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS hamberders_database;
+
+-- Create and use the burgers_db --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the burgers table in the burgers_db --
+CREATE TABLE burgers
+(
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (30) NOT NULL ,
+    devoured BOOLEAN DEFAULT false NOT NULL,
+    createdAt DATETIME DEFAULT now
+    () NOT NULL,
+    updatedAt DATETIME DEFAULT now
+    () NOT NULL,
+    PRIMARY KEY
+    (id)
+)
