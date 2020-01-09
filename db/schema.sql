aa2e3a66f75a2233239169d13427a07cb583f29a
Mark Sirany <marksirany@me.com>
2020-01-08 19:42:30 -0600

    edited sql content

--- db/schema.sql
+++ db/schema.sql
@@ -8,13 +8,9 @@ USE burgers_db;
 -- Create the burgers table in the burgers_db --
 CREATE TABLE burgers
 (
-    id INTEGER NOT NULL
-    AUTO_INCREMENT,
-    burger_name VARCHAR
+    id INTEGER NOT NULL AUTO_INCREMENT
+    burger_name VARCHAR(30),
     (30) NOT NULL ,
-    devoured BOOLEAN DEFAULT false NOT NULL,
-    createdAt DATETIME DEFAULT now() NOT NULL,
-    updatedAt DATETIME DEFAULT now() NOT NULL,
-    PRIMARY KEY
-    (id)
-)
+    devoured BOOLEAN NOT NUll default false,
+    PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,16 +2,16 @@ USE burgers_db;
 
 -- https://www.mysqltutorial.org/mysql-datetime/ --
 INSERT INTO burgers
-    (burger_name, devoured, createdAt, updatedAt)
+    (burger_name, devoured)
 VALUES
-    ("Taco Burger", false, now(), now());
+    ("Taco Burger", false);
 
 INSERT INTO burgers
-    (burger_name, devoured, createdAt, updatedAt)
+    (burger_name, devoured)
 VALUES
-    ("Veggie", false, now(), now());
+    ("Veggie", false);
 
 INSERT INTO burgers
-    (burger_name, devoured, createdAt, updatedAt)
+    (burger_name, devoured)
 VALUES
-    ("Turkey Burger", true, now(), now());
+    ("Turkey Burger", false);
