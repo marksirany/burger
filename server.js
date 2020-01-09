e3adde2f7bb8e98fc71dd41410fc111b5c067ba1
Mark Sirany <marksirany@me.com>
2020-01-08 19:58:52 -0600

    added new code to server.js

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
+const express = require("express")
 
-// DECLARE REQUIREMENTS //
-const db = require("./models");
-const express = require("express");
-
-// SETUP EXPRESS //
-
-const app = express();
 const PORT = process.env.PORT || 8080;
 
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// CREATE ROUTES //
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
-app.use(express.static('public'))
-require("./routes/api-routes")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// SYNCING MODELS AND START EXPRESS //
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller");
 
+app.use(routes);
 
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
-
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
